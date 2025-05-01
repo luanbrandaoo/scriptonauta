9232e2704ec680bad6a139c3486d717b97deb308
Luan Brandão <97369040+luanbrandaoo@users.noreply.github.com>
2025-05-01 14:57:38 +0000

    add login page

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,6 +4,7 @@ import Error404 from './pages/Error404';
 import DashboardPage from './pages/DashboardPage';
 import PlanetPage from './pages/PlanetPage';
 import ActivitiesPage from './pages/ActivitiesPage';
+import LoginPage from './pages/LoginPage';
 
 function AppRoutes() {
     return (
@@ -11,6 +12,7 @@ function AppRoutes() {
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/404" element={<Error404 />} />
+                <Route path="/login" element={<LoginPage />} />
                 <Route path="/dashboard" element={<DashboardPage />} />
                 <Route path="/:planetId" element={<PlanetPage />} />
                 <Route path="/:planetId/activities" element={<ActivitiesPage />} />
